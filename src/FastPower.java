18ad8048c49a8754707af04267bcc992ce26710b
saianuragthallamudi <saianuragthallamudi@gmail.com>
2024-04-11 18:57:06 +0530

    Tower Of Hanoi

--- src/FastPower.java
+++ src/FastPower.java
@@ -1,5 +1,5 @@
 public class FastPower {
-    public static int power(int x,int n)
+    public static long power(long x,long n)
     {
         if(n == 0)
         {
@@ -20,6 +20,7 @@ public class FastPower {
 
     public static void main(String[] args) {
         int x = 2;
-        System.out.println(power(3,4));
+        long val = power(244140,9);
+        System.out.println(val);
     }
 }
---
+++ src/TowerOfHanoi.java
@@ -0,0 +1,24 @@
+public class TowerOfHanoi {
+
+    public static void Move(char src,char dest, char help, int disks)
+    {
+        String str = "Move disk "+disks+" from "+src+" to "+dest;
+        if(disks == 1)
+        {
+            System.out.println(str);
+            return;
+        }
+        Move(src,help,dest,disks-1);
+        System.out.println(str);
+        Move(help,dest,src,disks-1);
+    }
+
+    public static void main(String[] args) {
+        int disks = 3;
+        char src = 'A';
+        char dest = 'C';
+        char help = 'B';
+
+        Move(src,dest,help,disks);
+    }
+}
